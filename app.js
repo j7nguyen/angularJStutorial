63b157d36f41957b2b6a45b19e746748928edcb1
Joseph Nguyen <vietjew@gmail.com>
2014-12-16 15:37:25 -0800

    Added more controllers and whatnot

--- app.js
+++ app.js
@@ -4,6 +4,28 @@
 	app.controller('StoreController', function() {
 		this.products = gems;
 	});
+
+	app.controller('PanelController', function() {
+		this.tab = 1;
+		
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+	
+	app.controller('GalleryController', function() {
+		this.current = 0;
+		
+		this.setCurrent = function(selection) {
+			this.current = selection;
+		};
+		
+		
+	});
 	
 	var gems = [
 		{
@@ -14,6 +36,13 @@
 			soldOut: false,
 			images: [
 				"images/dd1.jpeg"
+			],
+			reviews: [
+				{
+					stars: 4,
+					body: "This was a great gem!",
+					author: "bigguy@ether.com"
+				}
 			]
 		},
 		{
@@ -24,4 +53,6 @@
 			images: []
 		}
 	]
+	
+
 })();
