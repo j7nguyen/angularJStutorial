4b5dc20c3cc7a3afe6770041f9048729adf58ef1
Joseph Nguyen <vietjew@gmail.com>
2014-12-18 01:06:53 -0800

    More refactoring with custom directives

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
-  });
+	app.directive('productGallery', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-gallery.html',
+			controller: function() {
+		    this.current = 0;
+		    this.setCurrent = function(imageNumber){
+	      	this.current = imageNumber || 0;
+		    };
+			},
+			controllerAs: 'gallery'
+		};
+	});
 
   app.controller('StoreController', function() {
     this.products = gems;
@@ -47,6 +54,32 @@
 			restrict: 'A',
 			templateUrl: 'product-specs.html'
 		}
+	});
+	
+	app.directive("productPanels", function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function() {
+		    this.tab = 1;
+
+		    this.isSet = function(checkTab) {
+		      return this.tab === checkTab;
+		    };
+
+		    this.setTab = function(setTab) {
+		      this.tab = setTab;
+		    };
+			},
+			controllerAs: 'tab'
+		};
+	});
+
+	app.directive("productReviews", function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-reviews.html',
+		};
 	});
 
   var gems = [
