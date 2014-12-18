c38a85fab7e49de483a17de4b3036182921285bd
Joseph Nguyen <vietjew@gmail.com>
2014-12-18 01:16:11 -0800

    Refactored using modules

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
-
-	app.directive('productGallery', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'product-gallery.html',
-			controller: function() {
-		    this.current = 0;
-		    this.setCurrent = function(imageNumber){
-	      	this.current = imageNumber || 0;
-		    };
-			},
-			controllerAs: 'gallery'
-		};
-	});
+  var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('StoreController', function() {
     this.products = gems;
@@ -42,45 +28,6 @@
 
   });
 
-  app.directive("productDescription", function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-description.html'
-    };
-  });
-
-	app.directive("productSpecs", function() {
-		return {
-			restrict: 'A',
-			templateUrl: 'product-specs.html'
-		}
-	});
-	
-	app.directive("productPanels", function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'product-panels.html',
-			controller: function() {
-		    this.tab = 1;
-
-		    this.isSet = function(checkTab) {
-		      return this.tab === checkTab;
-		    };
-
-		    this.setTab = function(setTab) {
-		      this.tab = setTab;
-		    };
-			},
-			controllerAs: 'tab'
-		};
-	});
-
-	app.directive("productReviews", function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'product-reviews.html',
-		};
-	});
 
   var gems = [
     {
